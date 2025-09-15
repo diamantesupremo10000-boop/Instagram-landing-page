5d97659a0efa1ed8409629c33a1019df86235330
Carlos Hernández <diamante.supremo.10000@gmail.com>
2025-09-15 13:25:23 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function mostrarMensaje() {
-  alert("¡Excelente decisión! Una landing page profesional hará crecer tu negocio 🚀, Añádeme como contacto en WhatsApp. https://wa.me/qr/ST732IXK4B6MB1");
+  alert("¡Excelente decisión! Una landing page profesional hará crecer tu negocio 🚀, Añádeme como contacto en WhatsApp: +529131096378");
 }
